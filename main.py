ddc9d7cf936c186abde38c1d331ec3b42c8e1269
paulomoreiraKW <paulomoreira@kwportugal.pt>
2026-04-28 11:48:57 +0100

    Fix markdown formatting in main.py

--- main.py
+++ main.py
@@ -262,7 +262,7 @@ elif st.session_state.page == "DETALHE":
         exit_base = safe_float(row.get("Preco_Exit", 0))
         invest_total = safe_float(row.get("Investimento_Total", 0))
      
-     st.markdown(f"""
+        st.markdown(f"""
                 <div class="white-solid-box" style="min-height:380px;">
                     <img src="{row.get('Capa_Manual', '')}" style="width:100%; border-radius:10px; margin-bottom:10px;">
                     <b style="font-size:16px; display: block;">{row.get('Tipo')}</b>
